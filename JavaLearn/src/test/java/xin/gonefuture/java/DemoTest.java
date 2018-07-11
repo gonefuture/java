a0173f23ec95255c6d3fb79923392b86983560ce
gonefuture <gonefuture@qq.com>
2018-07-11 14:33:08 +0800

    add some demo for OIO and NIO

---
+++ JavaLearn/src/test/java/xin/gonefuture/java/DemoTest.java
@@ -0,0 +1,21 @@
+/**  
+ * 
+ * @Title:  DemoTest.java   
+ * @Package xin.gonefuture.java   
+ * @Description:    TODO(用一句话描述该文件做什么)   
+ * @author: qianweijian gonefuture@qq.com   
+ * @date:   2018年7月9日 下午4:16:35   
+ * @version V1.0 
+ */
+package xin.gonefuture.java;
+
+/**
+ * @author gonefuture
+ *
+ */
+public class DemoTest {
+	
+	
+
+}
+
---
+++ JavaLearn/src/test/java/xin/gonefuture/java/netty/transmission/NettyOIOServerTest.java
@@ -0,0 +1,32 @@
+/**  
+ * 
+ * @Title:  NettyOIOServer.java   
+ * @Package xin.gonefuture.java.netty.transmission   
+ * @Description:    TODO(用一句话描述该文件做什么)   
+ * @author: qianweijian gonefuture@qq.com   
+ * @date:   2018年7月10日 下午12:11:36   
+ * @version V1.0 
+ */
+package xin.gonefuture.java.netty.transmission;
+
+
+
+import org.junit.Test;
+
+/**
+ * @author gonefuture
+ *
+ */
+public class NettyOIOServerTest {
+	
+	@Test
+	public void testNettyOioServer() {
+		NettyOIOServer nettyOIOServer = new NettyOIOServer();
+		try {
+			nettyOIOServer.server(9999);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
