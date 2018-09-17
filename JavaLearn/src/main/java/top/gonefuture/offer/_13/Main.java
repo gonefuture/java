6379b08766718bfab1da72319ca501ee671ea19d
gonefuture <gonefuture@qq.com>
2018-09-17 22:26:37 +0800

    12

--- JavaLearn/src/main/java/top/gonefuture/offer/_13/Main.java
+++ JavaLearn/src/main/java/top/gonefuture/offer/_13/Main.java
@@ -1,12 +1,27 @@
 package top.gonefuture.offer._13;
 
+import java.util.Scanner;
+
 /**
  * @author gonefuture  gonefuture@qq.com
  * time 2018/8/30 16:45
  * @version 1.00
- * Description: 在O(1)时间删除链表结点
+ * Description:
  *
+ * 旧版：
+ * 在O(1)时间删除链表结点
  * 可以将下一个结点的数据覆盖被删除节点，然后指向下下个结点
+ *
+ *   试题13:机器人的运动范围
+ *   (0,0)开始,当k为18时,不能进入数位之和大于k的格子, k为18时,进入(35,37),因为3+5+3+1=18
  */
 public class Main {
+
+
+    public void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] maze = new int[m][n];
+    }
 }
